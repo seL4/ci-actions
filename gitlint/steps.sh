7ddce2da2e3464756724d737cf0c0780a544d9bd
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-06-12 19:26:39 +0800

    Provide config options
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

--- gitlint/steps.sh
+++ gitlint/steps.sh
@@ -18,9 +18,29 @@ checkout.sh
 # fetch pull request base
 fetch-base.sh
 
+# determine .gitlint config
+if [ ! -z ${INPUT_CONFIG+x} ]
+then
+  echo "Using arg ${INPUT_CONFIG}"
+  CONFIG="--config ${INPUT_CONFIG}"
+elif [ ! -r .gitlint ]
+then
+  . ${SCRIPTS}/fetch-sel4-tools.sh
+  CONFIG_PATH="misc/.gitlint"
+  CONFIG="--config ${SEL4_TOOLS}/${CONFIG_PATH}"
+  echo "Using default sel4_tools/${CONFIG_PATH}"
+else
+  echo "Using target repo .gitlint"
+fi
 echo "::endgroup::"
 
 echo
 
 HEAD_SHA=$(jq -r '.pull_request.head.sha' ${GITHUB_EVENT_PATH})
-gitlint --commits ${GITHUB_BASE_REF}..${HEAD_SHA} lint
+RANGE=${GITHUB_BASE_REF}..${HEAD_SHA}
+
+echo "Checking the following commits:"
+git log --oneline ${RANGE}
+
+echo
+gitlint ${CONFIG} --commits ${RANGE} lint && echo "Gitlint successful!"

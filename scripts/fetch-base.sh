03a3e48cc4a2c78da32f092151f815e349820136
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-06-12 18:23:24 +0800

    Factor out base ref fetch
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ scripts/fetch-base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Fetches all commits between HEAD and GITHUB_BASE_REF (base of a pull request)
+# Assumes working dir of repo left by checkout.sh
+
+echo "Fetching ${GITHUB_BASE_REF}"
+git fetch -q --no-tags origin +refs/heads/${GITHUB_BASE_REF}:refs/heads/${GITHUB_BASE_REF}
+git fetch -q --no-tags --shallow-exclude ${GITHUB_BASE_REF}

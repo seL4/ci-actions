836b2287a91777bed31b82cf3410b28b1f515d4a
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-06-13 18:07:47 +0800

    Add bashisms action
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ bashisms/README.md
@@ -0,0 +1,37 @@
+<!--
+  Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+  SPDX-License-Identifier: CC-BY-SA-4.0
+-->
+
+# Shell script check action
+
+This action runs the check for [portable shell code][1] from
+<https://github.com/seL4/sel4_tools> on pull requests.
+
+[1]: https://github.com/seL4/seL4_tools/tree/master/misc/is-valid-shell-script
+
+## Content
+
+The main action happens in [`steps.sh`](steps.sh), the JavaScript entry point
+just calls this script.
+
+## Arguments
+
+None
+
+## Example
+
+Put this into a `.github/workflows/` yaml file, e.g. `style.yml`:
+
+```yaml
+name: Style
+
+on: [pull_request]
+
+jobs:
+  style:
+    name: Shell
+    runs-on: ubuntu-latest
+    steps:
+    - uses: lsf37/ci-actions/bashisms@master
+```
---
+++ bashisms/steps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -e
+
+[ -z "${GITHUB_BASE_REF}" ] \
+  && (echo "Action only works on pull requests."; exit 1)
+
+echo "::group::Setting up"
+install-python.sh
+
+echo "Installing seL4 python deps"
+pip install -q sel4-deps
+
+echo "Installing astyle"
+sudo apt-get install -qq devscripts > /dev/null
+
+. ${SCRIPTS}/fetch-sel4-tools.sh
+
+checkout.sh
+# fetch pull request base if PR
+[ -n "${GITHUB_BASE_REF}" ] && fetch-base.sh
+
+echo "::endgroup::"
+
+echo
+echo "Checking the following files:"
+echo "$(git diff --name-only ${GITHUB_BASE_REF} test-revision)"
+echo
+git diff -z --name-only ${GITHUB_BASE_REF} test-revision | xargs -0 \
+  ${SEL4_TOOLS}/misc/is-valid-shell-script && echo "Check successful!"

4c9d97f0b0b16fe820984b17eea624dba9b5d9cf
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-08-10 16:15:37 +0800

    run-proofs: make run-proofs work for push and PR (#28)
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ scripts/fetch-branch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Fetches the branch in ${GITHUB_REF} into a repo manifest checkout
+
+# Assumes a repo manifest checkout, and current working dir in the repo
+# to fetch the branch for.
+
+URL="https://github.com/${GITHUB_REPOSITORY}.git"
+
+echo "Fetching ${GITHUB_REF} from ${PR_URL}"
+git fetch -q --depth 1 ${PR_URL} ${GITHUB_REF}:${GITHUB_REF}
+git checkout -q ${GITHUB_REF}

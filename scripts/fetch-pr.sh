aafaafd0d98699172ddb3c2ba32e428fb118d11d
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-08-27 13:38:11 +0800

    scritps: fetch-pr should use fetch, not pull
    Using `pull` tries an automatic merge if the ref is not compatible
    with base, which sometimes happens when people user the wrong
    base for a pull request. Just fetching the PR BASE_REF and REF and
    using `checkout` instead should avoid this.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

--- scripts/fetch-pr.sh
+++ scripts/fetch-pr.sh
@@ -13,16 +13,7 @@ PR_URL="https://github.com/${GITHUB_REPOSITORY}.git"
 
 echo "Fetching ${GITHUB_BASE_REF} from ${PR_URL}"
 git fetch -q --depth 1 ${PR_URL} ${GITHUB_BASE_REF}:${GITHUB_BASE_REF}
-git checkout -q ${GITHUB_BASE_REF}
 
 echo "Fetching ${GITHUB_REF} from ${PR_URL}"
-
-# make a hopefully locally unique branch name
-BRANCH="pull-requests/${GITHUB_SHA}"
-
-# This assumes that the PR is made to a branch compatible with
-# the repo manifest.
-git checkout -q -b ${BRANCH}
-
-# Pull in PR changes:
-git pull -q --no-tags "${PR_URL}" ${GITHUB_REF}
+git fetch -q "${PR_URL}" ${GITHUB_REF}:${GITHUB_REF}
+git checkout -q ${GITHUB_REF}

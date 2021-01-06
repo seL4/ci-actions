b077bd8f8f3a4c74eb2ecac1db2647d9a63e6c23
Gerwin Klein <gerwin.klein@data61.csiro.au>
2021-01-06 12:02:57 +1100

    thylint: action setup
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ thylint/steps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright 2021, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PATH="$DIR/../scripts":~/.local/bin:$PATH
+
+[ -z ${GITHUB_BASE_REF+x} ] && (echo "Action only works on pull requests"; exit 1)
+
+echo "::group::Setting up"
+install-python.sh
+
+checkout.sh
+# fetch pull request base
+fetch-base.sh
+echo "::endgroup::"
+
+echo
+echo "Checking the following files:"
+echo "$(git diff --name-only ${GITHUB_BASE_REF} test-revision)"
+echo
+git diff -z --name-only ${GITHUB_BASE_REF} test-revision | xargs -0 \
+  "$DIR/thylint.py" --json > annotations.json

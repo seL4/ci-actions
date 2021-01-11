dc32916eedee51272579acb9f1096a2a5ba6e3dd
Gerwin Klein <gerwin.klein@data61.csiro.au>
2021-01-11 18:50:02 +1100

    install-python: make sure to pick right version
    Previously, only `python` was set to `python3.7`, but `python3`
    remained on `python3.6`.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -8,5 +8,8 @@
 
 echo "Installing python 3.7"
 sudo apt-get install -qq python3.7 > /dev/null
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+sudo update-alternatives --config python3
+python3 --version
 pip3 install -q --upgrade pip setuptools wheel

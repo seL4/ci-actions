247c11e59552d683daf772dc61899d5f536290cf
Gerwin Klein <kleing@unsw.edu.au>
2021-03-29 10:07:05 +1100

    provide manifest option for repo
    Currently specifically for the verification manifest, defaulting
    to `devel.xml`
    
    Signed-off-by: Gerwin Klein <kleing@unsw.edu.au>

--- scripts/checkout-manifest.sh
+++ scripts/checkout-manifest.sh
@@ -12,10 +12,11 @@ set -e
 : ${MANIFEST_URL:="https://github.com/seL4/verification-manifest.git"}
 : ${REPO_DEPTH:=1}
 : ${REPO_BRANCH:="master"}
+: ${REPO_MANIFEST:="devel.xml"}
 
 : ${REPO:="repo"}
 
-# repo expects git to be set up; provid defaults if they don't exist
+# repo expects git to be set up; provide defaults if they don't exist
 git config user.name > /dev/null || \
   git config --global user.name "repo"
 git config user.email > /dev/null || \
@@ -25,5 +26,5 @@ git config color.ui > /dev/null || \
 
 echo "Starting repo checkout.."
 
-$REPO init --depth ${REPO_DEPTH} -b ${REPO_BRANCH} -u "${MANIFEST_URL}"
+$REPO init --depth ${REPO_DEPTH} -m ${REPO_MANIFEST} -b ${REPO_BRANCH} -u "${MANIFEST_URL}"
 $REPO sync

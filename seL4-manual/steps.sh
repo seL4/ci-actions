6a19531adb6804f3de475a23fbefbacb1a35ecd3
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-06-17 16:51:57 +1000

    Docker action for building the seL4 manual
    The action builds the PDF in draft mode.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ seL4-manual/steps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright 2021, Proofcraft Pty Ltd
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# Docker entrypoint for seL4 manual test
+
+set -e
+
+# actions:
+echo "::group::Setting up"
+checkout.sh
+echo "::endgroup::"
+
+# start test
+
+cd manual
+# set draft mode:
+sed -i '~'"s/%\\\\Drafttrue/\\\\Drafttrue/" manual.tex
+make

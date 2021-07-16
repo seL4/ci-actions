f07be9b77e0304f1c3b9443e8b8f7a07414396b2
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-07-16 13:26:59 +1000

    Add a GitHub action for seL4 hardware builds
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ sel4test-hw/build.py
@@ -0,0 +1,42 @@
+# Copyright 2021, Proofcraft Pty Ltd
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+Parse builds.yml and run sel4test hardware builds on each of the build definitions.
+
+Expects seL4-platforms/ to be co-located or otherwise in the PYTHONPATH.
+"""
+
+from builds import Build, run_build_script, run_builds, load_builds, default_junit_results
+from pprint import pprint
+
+import os
+import sys
+
+
+def run_build(manifest_dir: str, build: Build):
+    """Run one hardware build."""
+
+    script = [
+        ["../init-build.sh"] + build.settings_args(),
+        ["ninja"],
+        ["tar", "czf", f"../{build.name}-images.tar.gz", "images/"]
+    ]
+
+    return run_build_script(manifest_dir, build.name, script)
+
+
+def build_filter(build: Build) -> bool:
+    return not build.get_platform().disabled
+
+
+# If called as main, run all builds from builds.yml
+if __name__ == '__main__':
+    builds = load_builds(os.path.dirname(__file__) + "/builds.yml", filter_fun=build_filter)
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--dump':
+        pprint(builds)
+        sys.exit(0)
+
+    sys.exit(run_builds(builds, run_build))

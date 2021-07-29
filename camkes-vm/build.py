2088df64af4194563b3e2e270b982a92a67b6f26
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-07-29 15:35:09 +1000

    camkes-vm: add Arm builds
    These used to run in a separate test on Bamboo, which used the retired
    camkes-arm-vm-manifest. Now unified in here.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- camkes-vm/build.py
+++ camkes-vm/build.py
@@ -24,17 +24,34 @@ import sys
 def run_build(manifest_dir: str, build: Build):
     """Run one CAmkES VM test."""
 
-    build.files = build.get_platform().image_names(build.get_mode(), "capdl-loader")
-    build.settings['CAMKES_VM_APP'] = build.name
+    plat = build.get_platform()
+
+    build.files = plat.image_names(build.get_mode(), "capdl-loader")
+    build.settings['CAMKES_VM_APP'] = build.app or build.name
     del build.settings['BAMBOO']    # not used in this test, avoid warning
-    del build.settings['PLATFORM']  # not used in this test, avoid warning
+
+    if plat.arch == 'x86':
+        del build.settings['PLATFORM']  # not used for x86 in this test, avoid warning
+        # build.req set by default in builds.yml for x86
+    else:
+        build.req = plat.req or plat.name.lower()
 
     script = [
         ["../init-build.sh"] + build.settings_args(),
         ["ninja"],
         ["tar", "czf", f"../{build.name}-images.tar.gz", "images/"],
-        ["echo", f"hardware run for {build.req}, skipping"]
     ]
+
+    if plat.simulation_binary:
+        script.append(
+            ["bash", "-c",
+             f"expect -c 'spawn ./simulate; set timeout 3000; expect \"{build.success}\"'"]
+        )
+
+    if not plat.disabled:
+        script.append(["echo", f"Hardware run for {build.req}, skipping"])
+    else:
+        script.append(["echo", f"Platform for build {build.name} disabled, skipping"])
 
     return run_build_script(manifest_dir, build.name, script)
 

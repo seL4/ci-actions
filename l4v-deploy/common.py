fca62813e7efce772fe35b9f359111d8e46d78d0
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-08-11 16:16:36 +1000

    add action for l4v manifest deployment
    This contains code for deployment after l4v proof runs and after
    seL4 preprocess success.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ l4v-deploy/common.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020, Data61, CSRIO
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Common utils.
+
+import os
+import subprocess
+
+
+def run_command(*args, **kwargs):
+    '''Trivial wrapper for subprocess.check_output'''
+    return subprocess.check_output(*args, **kwargs)
+
+
+def loud_command(*args, **kwargs):
+    '''Echo command then run it'''
+    print("[Command] %s" % args[0])
+    return run_command(*args, **kwargs)
+
+
+def indent(s, indent='    '):
+    '''Indent all lines in a string'''
+    return '\n'.join(indent + l for l in s.splitlines())
+
+
+def format_commit_message(msg):
+    '''Add a standard header and footer to a commit message'''
+    msg = "[CI] " + msg
+    return msg
+
+# Common settings.
+
+
+git_commit_user = 'seL4 CI'
+git_commit_email = 'ci@sel4.systems'
+
+
+def set_repo_email(repo='.', user=git_commit_user, email=git_commit_email):
+    loud_command(['git', 'config', 'user.name', user], cwd=repo)
+    loud_command(['git', 'config', 'user.email', email], cwd=repo)

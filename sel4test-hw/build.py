74e9639bf9e4043b111f94734dcc910ca06cddf1
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-11-19 15:21:10 +1100

    seltest-hw-run: produce build matrix
    Instead of replicating the build matrix in the workflow files, we
    generate it here from platforms.yml and builds.yml.
    
    Closes #168
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- sel4test-hw/build.py
+++ sel4test-hw/build.py
@@ -8,10 +8,14 @@ Parse builds.yml and run sel4test hardware builds and runs on each of the build 
 Expects seL4-platforms/ to be co-located or otherwise in the PYTHONPATH.
 """
 
-from builds import Build, run_build_script, run_builds, load_builds, junit_results, release_mq_locks
-from builds import SKIP
-from pprint import pprint
+from builds import Build, run_build_script, run_builds, load_builds, junit_results
+from builds import release_mq_locks, SKIP
+from platforms import Platform
 
+from pprint import pprint
+from typing import List
+
+import json
 import os
 import sys
 
@@ -70,12 +74,51 @@ def build_filter(build: Build) -> bool:
     return True
 
 
+def to_json(builds: List[Build]) -> dict:
+    """Return a GitHub build matrix per enabled hardware platform as GitHub set-output."""
+
+    def run_for_plat(plat: Platform) -> List[dict]:
+        if plat.disabled or plat.no_hw_build:
+            return []
+
+        # separate runs for each compiler on arm
+        if plat.arch == 'arm':
+            return [
+                {"platform": plat.name, "march": plat.march, "compiler": "gcc"},
+                {"platform": plat.name, "march": plat.march, "compiler": "clang"},
+            ]
+
+        # no clang for RISC-V yet
+        if plat.arch == 'riscv':
+            return [
+                {"platform": plat.name, "march": plat.march, "compiler": "gcc"},
+            ]
+
+        # separate runs for each compiler + mode on x86, because we have more machines available
+        if plat.arch == 'x86':
+            return [
+                {"platform": plat.name, "march": plat.march, "compiler": "gcc", "mode": 32},
+                {"platform": plat.name, "march": plat.march, "compiler": "clang", "mode": 32},
+                {"platform": plat.name, "march": plat.march, "compiler": "gcc", "mode": 64},
+                {"platform": plat.name, "march": plat.march, "compiler": "clang", "mode": 64},
+            ]
+
+    platforms = set([b.get_platform() for b in builds])
+    matrix = {"include": [run for plat in platforms for run in run_for_plat(plat)]}
+
+    return "::set-output name=matrix::" + json.dumps(matrix)
+
+
 # If called as main, run all builds from builds.yml
 if __name__ == '__main__':
     builds = load_builds(os.path.dirname(__file__) + "/builds.yml", filter_fun=build_filter)
 
     if len(sys.argv) > 1 and sys.argv[1] == '--dump':
         pprint(builds)
+        sys.exit(0)
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--matrix':
+        print(to_json(builds))
         sys.exit(0)
 
     if len(sys.argv) > 1 and sys.argv[1] == '--hw':

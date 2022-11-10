448e03a3d9f3312aac5cafa4c5a56417f9c5a552
Gerwin Klein <gerwin.klein@proofcraft.systems>
2022-11-10 14:57:39 +1100

    scripts: new-line unescape manually
    %0A, %0D, and %25 were previously un-escaped automatically for
    GITHUB_OUTPUT settings, but that is no longer the case with the new
    output file mechanism.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- scripts/nl-escape.sh
+++ scripts/nl-escape.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 
-# Escape %, \n, \r for GitHub value setting. It is un-escaped automatically by GH.
+# Escape %, \n, \r for GitHub value setting. Use nl-unescape.sh to undo.
 
 # portable sed is no good for newline replacement, but there is always perl
 perl -pe 's/%/%25/g' | perl -pe 's/\n/%0A/g' | perl -pe 's/\r/%0D/g'

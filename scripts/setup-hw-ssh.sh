8d2ca0cbc0e8d8be9fb303e3dd0e734208df8334
Gerwin Klein <gerwin.klein@proofcraft.systems>
2023-07-18 17:10:42 +0700

    scripts: update ts server ID
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- scripts/setup-hw-ssh.sh
+++ scripts/setup-hw-ssh.sh
@@ -8,7 +8,7 @@
 # set up ssh for access to UNSW machine queue
 
 mkdir -p ~/.ssh
-echo "login.trustworthy.systems,129.94.173.83 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGJaDmM3/MtDmmLMzh39/xbaWbkiX0RJCGb/aNrlaaqdkW10l20hbzs9zJ1rp1USv2y4YszTXwbJCH7J7PZNqeA=" >> ~/.ssh/known_hosts
+echo "login.trustworthy.systems ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD111xPT6mKt1s+wJvXIGwUXaebbM/B1GE7ztMUgKBqySbO/5AXXFUr/xflvSluH3lYG5tTpGwPYbJyHOmnJGLY=" >> ~/.ssh/known_hosts
 echo "tftp.keg.cse.unsw.edu.au ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEj7X6doSoop91gTvBD7L4O7VGwCO5pLNsu5YAGS1L64MJqo+3wTYgFRdMWTM0hL3YN+1sSabJPICJzKk0EJxkg=" >> ~/.ssh/known_hosts
 
 echo "Host *" >> ~/.ssh/config

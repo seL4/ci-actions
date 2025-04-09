42d2d8f5055b3221d4636587deb4a440348a3825
julia <git.ts@trainwit.ch>
2025-04-09 12:42:00 +1000

    python: pin wheel package to match setuptools
    https://github.com/pypa/wheel/issues/660
    
    Signed-off-by: julia <git.ts@trainwit.ch>

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -11,4 +11,9 @@ sudo apt-get install -qq python3.10 > /dev/null
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 python3 --version
-pip3 install -q --upgrade pip setuptools==70.0.0 launchpadlib wheel
+
+# setuptools and wheel versions must be matched to working releases
+# we need an old version of setuptools (see PR seL4/ci-actions#381)
+# and newer versions of wheel removes the bdist_wheel implementation
+# that setuptools used to use, but no longer does in newer versions.
+pip3 install -q --upgrade pip setuptools==70.0.0 wheel==0.45.1 launchpadlib

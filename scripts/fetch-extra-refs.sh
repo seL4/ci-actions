076165b28649bcc6fcd8cadc549bdad77cc6d4ab
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-07-16 20:15:06 +1000

    scripts: allow "Test with" outside manifests
    Generalise fetch-extra-refs.sh so that it can pull branches or PRs for
    plain git clones that are not part of a repo manifest checkout.
    
    Use an env variable EXTRA_REFS_PATHS for org/repo to path mapping in
    that case.
    
    Make sure repo-util has empty stdout when repo manifest is not present.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- scripts/fetch-extra-refs.sh
+++ scripts/fetch-extra-refs.sh
@@ -8,6 +8,37 @@
 # Expects INPUT_EXTRA_REFS to be a space separated list of:
 #   org/repo#num  - pull request (fetched from refs/pull/<num>/head)
 #   org/repo@ref  - branch name or commit SHA (fetched as <ref>)
+#
+# If EXTRA_REFS_PATHS is set, assume plain git clones under these paths.
+# Otherwise assume a repo manifest checkout. Specs in EXTRA_REFS_PATHS take
+# precedence if both are present.
+#
+# Expects EXTRA_REFS_PATHS to be unset or a space separated list of:
+#   org/repo=path - path of that repo, relative to the current directory
+
+# Print the path of repo $1 ("org/repo") or nothing if not present.
+repo_dir() {
+  # treat repo names as case-insensitive for comparison, as in repo-util
+  NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+  for P in ${EXTRA_REFS_PATHS}
+  do
+    # extract the part before "=" (org/repo), case insensitive
+    KEY=$(echo "${P%=*}" | tr '[:upper:]' '[:lower:]')
+    if [ "${KEY}" = "${NAME}" ]
+    then
+      # return the part after "=" (path)
+      echo "${P#*=}"
+      return
+    fi
+  done
+
+  # fall through to repo-util, we could in theory do both (plain checkout plus manifest)
+  if [ -d .repo ]
+  then
+    repo-util path "$1" 2>/dev/null
+  fi
+}
 
 if [ -z "${INPUT_XML}" ] && [ -n "${INPUT_EXTRA_REFS}" ]
 then
@@ -35,6 +66,13 @@ then
         ;;
     esac
 
+    DIR=$(repo_dir "${REPO}")
+    if [ -z "${DIR}" ]
+    then
+      echo "Skipping ${R}: ${REPO} is not part of this checkout/manifest"
+      continue
+    fi
+
     REPO_PATH="github.com/${REPO}.git"
 
     if [ -n "${INPUT_TOKEN}" ]
@@ -45,7 +83,7 @@ then
       URL="https://${REPO_PATH}"
     fi
 
-    cd $(repo-util path ${REPO})
+    cd "${DIR}"
     echo "Fetching ${REF} from ${REPO_PATH}"
     git fetch -q --depth 1 ${URL} ${FETCH}
     git checkout -q ${CHECKOUT}

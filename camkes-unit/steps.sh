de290438983729d147003881b04a145b63e7fb7d
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-07-16 20:17:19 +1000

    camkes-unit: accept "Test with:" specs
    Use the new fetch-extra-refs.sh abilities from previous commit to make
    "Test with:" specs available for camkes-unit tests.
    
    Add GitHub token input for decreasing rate limits on PR description
    fetch.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- camkes-unit/steps.sh
+++ camkes-unit/steps.sh
@@ -13,12 +13,22 @@ git clone https://github.com/seL4/capdl
 mkdir camkes-tool
 cd camkes-tool
 checkout.sh
+cd - > /dev/null
+
+if [ "${GITHUB_EVENT_NAME}" = "pull_request_target" ] ||
+   [ "${GITHUB_EVENT_NAME}" = "pull_request" ]
+then
+  export INPUT_EXTRA_REFS="$(get-prs)"
+  export EXTRA_REFS_PATHS="seL4/capdl=capdl"
+  fetch-extra-refs.sh
+fi
 
 echo
 echo "Repo summary:"
-echo "camkes-tool: $(git rev-parse --short HEAD)"
-echo "      capdl: $(git -C ../capdl rev-parse --short HEAD)"
+echo "camkes-tool: $(git -C camkes-tool rev-parse --short HEAD)"
+echo "      capdl: $(git -C capdl rev-parse --short HEAD)"
 
+cd camkes-tool
 export PYTHONPATH=../capdl/python-capdl-tool
 echo "::endgroup::"
 
